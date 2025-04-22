f6205409f86f82062d4beb277080b83963210172
Igor Moura <igor.oliveira.moura@gmail.com>
2025-04-21 21:48:46 -0300

    feat: connectToRabbitMq

--- rabbitmq-chat-server/index.ts
+++ rabbitmq-chat-server/index.ts
@@ -1,20 +1,37 @@
-import express from 'express';
-import cors from "cors";
-import amqp, { Channel, Connection } from "amqplib";
-
+const express = require("express");
+const amqp = require("amqplib");
+const cors = require("cors");
 
 const app = express();
-const QUEUE_NAME = 'mensagens'
+const PORT = 3000;
+const RABBITMQ_URL = "amqp://localhost";
+const QUEUE = "messages";
+
+let channel = null;
+let connection = null;
+
+async function connectRabbitMQ() {
+  try {
+    connection = await amqp.connect(RABBITMQ_URL);
+    channel = await connection.createChannel();
+    await channel.assertQueue(QUEUE, { durable: false });
+
+    console.log("Conectado ao RabbitMQ");
+    
+    channel.consume(QUEUE, (msg) => {
+      if (msg) {
+        const message = msg.content.toString();
+        console.log("Mensagem recebida", message);
+        channel.ack(msg); // Acknowledge da mensagem
+      }
+    });
+
+  } catch (error) {
+    console.error("Falha ao conectar no RabbitMQ:", error);
+  }
+}
+
+connectRabbitMQ();
 
 app.use(cors());
 app.use(express.json());
-
-let channel: Channel;
-let connection: Connection;
-
-async function connectRabbitMQ() {
-    connection = await amqp.connect("amqp://localhost");
-    channel = await connection.createChannel();
-    await channel.assertQueue(QUEUE_NAME)
-    console.log("connected to rabbitmq")
-}
